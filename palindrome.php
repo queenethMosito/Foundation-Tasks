481cecee8940944c86cb4835623186b5726053b5
queenethMosito <70744785+queenethMosito@users.noreply.github.com>
2020-11-03 03:48:20 +0200

    Foundation Task 3

---
+++ palindrome.php
@@ -0,0 +1,32 @@
+<?php
+class Palindrome {
+    public static function isPalindrome($str) {
+        
+        //We are removing spaces from the string
+        $string = str_replace(' ', '', $str);
+        
+        //make everything lower case
+        $string = strtolower($string);
+        
+        //reverse the string
+        $reverse = strrev($string);
+        
+        if ($string == $reverse) {
+           return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+if (Palindrome::isPalindrome('Never Odd Or Even'))
+{
+    echo 'Palindrome';
+}
+else
+{
+    echo 'Not palindrome';
+}
+        
+?>
